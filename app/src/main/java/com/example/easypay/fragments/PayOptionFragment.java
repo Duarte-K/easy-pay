79ae664e30f018cb97dcf04a7ba45c21660bb581
Kaua Duarte <kfrutuoso49@gmail.com>
2021-12-09 09:41:37 -0300

    Escolha das cores, temas, strings principais e fontes usadas

---
+++ app/src/main/java/com/example/easypay/fragments/PayOptionFragment.java
@@ -0,0 +1,26 @@
+package com.example.easypay.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.easypay.R;
+
+public class PayOptionFragment extends Fragment {
+
+    public PayOptionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pay_option, container, false);
+    }
+}
