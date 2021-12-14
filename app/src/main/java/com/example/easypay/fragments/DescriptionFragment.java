8ca75d645c8e248e78b069e6f4fad47cc2af2a79
Kaua Duarte <kfrutuoso49@gmail.com>
2021-12-14 15:47:10 -0300

    Recuperação de valores dos campos e radioButton

--- app/src/main/java/com/example/easypay/fragments/DescriptionFragment.java
+++ app/src/main/java/com/example/easypay/fragments/DescriptionFragment.java
@@ -1,5 +1,6 @@
 package com.example.easypay.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,11 +11,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.easypay.R;
 import com.example.easypay.model.PayModel;
 import com.example.easypay.utils.MoneyTextWatcher;
+
+import java.time.temporal.TemporalAccessor;
+
+import cielo.orders.domain.Credentials;
+import cielo.sdk.order.OrderManager;
+import cielo.sdk.order.ServiceBindListener;
 
 
 public class DescriptionFragment extends Fragment {
@@ -22,6 +32,9 @@ public class DescriptionFragment extends Fragment {
     private Button btnEnviar;
     private PayModel pay;
     private EditText description, value;
+    private RadioGroup rgOption;
+    private String rbOpt;
+
 
 
     public DescriptionFragment() {
@@ -36,30 +49,50 @@ public class DescriptionFragment extends Fragment {
         btnEnviar = view.findViewById(R.id.btn_enviar);
         description = view.findViewById(R.id.tx_Description);
         value = view.findViewById(R.id.tx_Value);
+        rgOption = view.findViewById(R.id.rg_payOption);
         pay = new PayModel();
 
         value.addTextChangedListener(new MoneyTextWatcher(value));
 
+        Credentials credentials = new Credentials("", "");
+
+        rgOption.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.rb_credito){
+                    rbOpt = ((RadioButton) view.findViewById(rgOption.getCheckedRadioButtonId())).getText().toString();
+                }else{
+                    rbOpt = ((RadioButton) view.findViewById(rgOption.getCheckedRadioButtonId())).getText().toString();
+                }
+
+            }
+        });
+
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                InstallmentsFragment installmentsFragment = new InstallmentsFragment();
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                transaction.replace(R.id.frame_conteudo, installmentsFragment);
-                transaction.commit();
+                check();
+                if (check()){
+                    InstallmentsFragment installmentsFragment = new InstallmentsFragment();
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frame_conteudo, installmentsFragment);
+                    transaction.commit();
+                }
             }
         });
         return view;
     }
 
-    public void check(){
+    public boolean check(){
         String descpay = description.getText().toString();
         String valuePay = value.getText().toString();
 
         if(descpay.equals("")){
             description.setError("Preencha o campo para continuar");
+            return false;
         }else if(valuePay.equals("")){
             value.setError("Preencha o campo para continuar");
+            return false;
         }else{
             String valueFormat = value.getText().toString().substring(2);
             float valueFloat = 0;
@@ -70,8 +103,8 @@ public class DescriptionFragment extends Fragment {
             }
             pay.setDescription(descpay);
             pay.setPayValue(valueFloat);
-
+            pay.setPayOption(rbOpt);
         }
-
+        return true;
     }
 }
--- app/src/main/java/com/example/easypay/fragments/InstallmentsFragment.java
+++ app/src/main/java/com/example/easypay/fragments/InstallmentsFragment.java
@@ -3,16 +3,18 @@ package com.example.easypay.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.easypay.R;
 
 
 public class InstallmentsFragment extends Fragment {
-
+    private Button btnCancel;
     public InstallmentsFragment() {
         // Required empty public constructor
     }
@@ -21,6 +23,21 @@ public class InstallmentsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_installments, container, false);
+        View view = inflater.inflate(R.layout.fragment_installments, container, false);
+
+        btnCancel = view.findViewById(R.id.btn_cancel);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DescriptionFragment descriptionFragment = new DescriptionFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_conteudo, descriptionFragment);
+                transaction.commit();
+            }
+        });
+
+        return view;
     }
+
 }
